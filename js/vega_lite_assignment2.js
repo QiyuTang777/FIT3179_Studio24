cb96cfefbd0f2cefc63d9c08756824a101ed3ca5
QiyuTang777 <qtan0019@student.monash.edu>
2022-10-18 22:47:38 +1100

    Assignment2 test

---
+++ js/vega_lite_assignment2.js
@@ -0,0 +1,4 @@
+var spec2 = "js/world_population.json";
+vegaEmbed("#map", spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
